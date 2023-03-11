d9400e10e0bfebc271f5dc1639d153c8019238ca
hailm <hai.lemanh.1310@gmail.com>
2023-03-12 00:08:58 +0700

    feat: update

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php $data["title"] = "Home page"; ?>
 <?php $bodyClass = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
+<!--Slider-->
+<div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="autoplay: true; min-height: 300; max-height: 600">
 
+    <ul class="uk-slideshow-items">
+        <li>
+            <img src="images/image8.png" alt="" uk-cover="">
+        </li>
+        <li>
+            <img src="images/photo.jpg" alt="" uk-cover>
+        </li>
+        <li>
+            <img src="images/dark.jpg" alt="" uk-cover>
+        </li>
+        <li>
+            <img src="images/light.jpg" alt="" uk-cover>
+        </li>
+    </ul>
+
+    <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+
+</div>
+<!--/Slider-->
+
+<!---->
+<div class="uk-section">
+
+</div>
+<!--/-->
 <?php require "template-parts/layouts/footer.php"; ?>
