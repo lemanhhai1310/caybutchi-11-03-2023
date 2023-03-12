76b54698733674ea9f192e8d6aa9cf046ac83bf2
hailm <hai.lemanh.1310@gmail.com>
2023-03-12 14:54:58 +0700

    feat: page about

---
+++ gioithieu.php
@@ -0,0 +1,172 @@
+<?php $data["title"] = "Giới thiệu"; ?>
+<?php $isLogin = false ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<!--breadcrumb-->
+<div class="about__section1">
+    <div class="uk-container">
+        <ul class="uk-breadcrumb about__breadcrumb">
+            <li><a href=".">Trang chủ </a></li>
+            <li><span>Giới thiệu</span></li>
+        </ul>
+    </div>
+</div>
+<!--/breadcrumb-->
+
+<!--Thư ngỏ-->
+<div class="uk-background-norepeat uk-light uk-background-center-center uk-background-cover uk-position-relative" data-src="images/image10.png" uk-img>
+    <div class="about__thungo__gradient uk-position-cover"></div>
+    <div class="uk-position-relative uk-section">
+        <div class="uk-container">
+            <h2 class="uk-h2 about__thungo__title">Thư ngỏ</h2>
+        </div>
+    </div>
+</div>
+<!--/Thư ngỏ-->
+
+<!--Lời nói đầu...-->
+<div class="uk-section home__section">
+    <div class="uk-container">
+        <div class="uk-child-width-1-2@m uk-flex-middle uk-grid uk-grid-88-m" uk-grid>
+            <div class="uk-flex-last@m">
+                <div class="home__about__coverImg uk-cover-container uk-border-rounded uk-background-muted">
+                    <img src="images/Rectangle140378.png" alt="" uk-cover="">
+                    <canvas width="650" height="480"></canvas>
+                </div>
+            </div>
+            <div>
+                <article class="uk-article about__article">
+                    <h1 class="uk-article-title">Lời nói đầu...</h1>
+                    <p>Quý phụ huynh và các em học sinh thân mến,</p>
+                    <p>Cây Bút Nhí nổi bật là đơn vị tiên phong đưa xu hướng học trực tuyến với giáo viên và lộ trình chuẩn Quốc Tế để mang lại khóa học chất lượng cao dành cho học viên không chỉ tại Việt Nam mà còn đang sinh sống và làm việc tại nước ngoài.</p>
+                    <p>Dốc hết tâm huyết với học viên - mang lại những trải nghiệm tuyệt vời trong suốt quá trình theo học tỉ mỉ, tận tình, không lý do, không tư lợi hướng tới điều tốt nhất cho khách hàng và nhân viên cơ hội sẻ chia - Hợp tác cùng phát triển. Cây Bút Nhí  nổi bật là đơn vị tiên phong đưa xu hướng học trực tuyến với giáo viên và lộ trình chuẩn Quốc Tế để mang lại khóa học chất lượng cao dành cho học viên không chỉ tại Việt Nam mà còn đang sinh sống và làm việc tại nước ngoài.</p>
+                    <p>Dốc hết tâm huyết với học viên - mang lại những trải nghiệm tuyệt vời trong suốt quá trình theo học tỉ mỉ, tận tình, không lý do, không tư lợi hướng tới điều tốt nhất cho khách hàng và nhân viên cơ hội sẻ chia - Hợp tác cùng phát triển. Cây Bút Nhí  nổi bật là đơn vị tiên phong đưa xu hướng học trực tuyến với giáo viên và lộ trình chuẩn Quốc Tế để mang lại khóa học chất lượng cao dành cho học viên không chỉ tại Việt Nam mà còn đang sinh sống và làm việc tại nước ngoài.</p>
+                </article>
+            </div>
+        </div>
+
+        <div class="uk-child-width-1-2@m uk-flex-middle uk-grid uk-grid-88-m item__100-50" uk-grid>
+            <div class="">
+                <div class="home__about__coverImg uk-cover-container uk-border-rounded uk-background-muted">
+                    <img src="images/Rectangle140378.png" alt="" uk-cover="">
+                    <canvas width="650" height="480"></canvas>
+                </div>
+            </div>
+            <div>
+                <article class="uk-article about__article">
+                    <h1 class="uk-article-title">Về Cây Bút Nhí</h1>
+                    <p>Cây Bút Nhí nổi bật là đơn vị tiên phong đưa xu hướng học trực tuyến với giáo viên và lộ trình chuẩn Quốc Tế để mang lại khóa học chất lượng cao dành cho học viên không chỉ tại Việt Nam mà còn đang sinh sống và làm việc tại nước ngoài.</p>
+                    <p>Dốc hết tâm huyết với học viên - mang lại những trải nghiệm tuyệt vời trong suốt quá trình theo học tỉ mỉ, tận tình, không lý do, không tư lợi hướng tới điều tốt nhất cho khách hàng và nhân viên cơ hội sẻ chia - Hợp tác cùng phát triển. Cây Bút Nhí  nổi bật là đơn vị tiên phong đưa xu hướng học trực tuyến với giáo viên và lộ trình chuẩn Quốc Tế để mang lại khóa học chất lượng cao dành cho học viên không chỉ tại Việt Nam mà còn đang sinh sống và làm việc tại nước ngoài.Dốc hết tâm huyết với học viên - mang lại những trải nghiệm tuyệt vời trong suốt quá trình theo học tỉ mỉ, tận tình, không lý do.</p>
+                    <p>Dốc hết tâm huyết với học viên - mang lại những trải nghiệm tuyệt vời trong suốt quá trình theo học tỉ mỉ, tận tình, không lý do, không tư lợi hướng tới điều tốt nhất cho khách hàng và nhân viên cơ hội sẻ chia - Hợp tác cùng phát triển. </p>
+                </article>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/Lời nói đầu...-->
+
+<!--Câu chuyện hình thành và phát triển về Cây Bút Nhí-->
+<div class="about__cauchuyen__bg uk-section-xsmall uk-text-center">
+    <div class="uk-container">
+        <div class="about__cauchuyen__txt">Câu chuyện hình thành và phát triển về Cây Bút Nhí</div>
+    </div>
+</div>
+<!--/Câu chuyện hình thành và phát triển về Cây Bút Nhí-->
+
+<!--Timeline-->
+<div class="uk-section home__section">
+    <div class="uk-container" uk-scrollspy="target: > div > div; cls: uk-animation-slide-bottom-small; delay: 500">
+        <?php for ($i=1;$i<=4;$i++): ?>
+        <div class="uk-child-width-1-2@m uk-grid-divider uk-grid-large about__timeline__divider" uk-grid>
+            <div class="<?= ($i%2 === 0)?'uk-flex-last@m right':'left' ?>">
+                <div class="uk-position-relative">
+                    <div class="about__timeline__cardImg uk-card uk-card-body uk-card-default">
+                        <img class="uk-width" src="images/Rectangle140378.png" alt="">
+                    </div>
+                    <div class="about__timeline__dot">
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="about__timeline__title">Cây Bút Nhí</div>
+                <p class="about__timeline__desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+        </div>
+        <?php endfor; ?>
+    </div>
+</div>
+<!--/Timeline-->
+
+<!--Thành tựu nổi bật và đáng tự hào-->
+<div class="uk-section home__section">
+    <div class="uk-container">
+        <h2 class="uk-h2 uk-text-center about__title">Thành tựu nổi bật và đáng tự hào</h2>
+        <div class="item__80-40 uk-child-width-1-4@m uk-flex-center" uk-grid>
+            <?php
+            $data = array(
+                array(
+                    'title' => '5000+',
+                    'txt' => 'Học sinh đã và đang theo học tại trung tâm và trực tuyến ',
+                    'svg' => '<svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M41.25 2.50342C39.1942 2.50342 37.5049 4.19256 37.5049 6.24843V26.2435C37.5049 28.2994 39.1942 30.001 41.25 30.001H42.4951V36.2462C42.4939 36.4863 42.5618 36.7215 42.6907 36.9241C42.8197 37.1266 43.0043 37.2878 43.2224 37.3882C43.4405 37.4886 43.6829 37.5241 43.9206 37.4904C44.1584 37.4567 44.3814 37.3554 44.563 37.1983L52.959 30.001H73.7476C75.8034 30.001 77.5049 28.2995 77.5049 26.2435V6.24843C77.5049 4.19256 75.8034 2.50342 73.7476 2.50342H41.25ZM6.25 4.99365C4.19412 4.99365 2.50489 6.69504 2.50489 8.75092V48.751C2.50489 50.8069 4.19412 52.4961 6.25 52.4961H68.7476C70.8035 52.4961 72.5024 50.8069 72.5024 48.751V32.501H67.5024V46.2509C67.5011 46.5824 67.3682 46.8999 67.1329 47.1334C66.8975 47.3669 66.5791 47.4973 66.2476 47.496H8.75244C8.58809 47.497 8.42516 47.4656 8.27298 47.4035C8.12081 47.3414 7.98237 47.2499 7.86558 47.1343C7.7488 47.0186 7.65597 46.881 7.5924 46.7294C7.52883 46.5779 7.49577 46.4153 7.49512 46.2509V11.251C7.49449 11.0858 7.5266 10.9221 7.5896 10.7694C7.65259 10.6167 7.74522 10.4781 7.86213 10.3614C7.97904 10.2447 8.11792 10.1523 8.27073 10.0896C8.42355 10.0269 8.58727 9.99508 8.75244 9.99604H35.0049V6.24843C35.0049 5.8067 35.0965 5.38828 35.2393 4.99365H6.25ZM52.5464 8.75092C52.7606 8.75564 52.974 8.81668 53.1616 8.93431L63.1641 15.1916C63.9489 15.6811 63.9489 16.8238 63.1641 17.3132L53.1616 23.5607C52.329 24.0773 51.2527 23.4785 51.2524 22.4987V9.99632C51.2524 9.18807 51.9039 8.73098 52.5464 8.75092ZM25 14.9986C20.8666 14.9986 17.4976 18.3676 17.4976 22.501V26.246C17.4938 26.9411 18.0573 27.506 18.7524 27.5034V28.7485C18.7524 30.9592 19.7212 32.945 21.25 34.3198V37.5058H19.9976C15.865 37.5058 12.4965 40.8613 12.4951 44.9935H40.0024C40.0012 40.8613 36.635 37.5058 32.5024 37.5058H31.25V34.3222C32.7783 32.9474 33.7476 30.9608 33.7476 28.7485V27.5034C34.4427 27.5059 35.0065 26.9411 35.0024 26.246V22.501C35.0025 18.3676 31.6334 14.9986 27.5 14.9986H25ZM29.8169 25.3354L31.2451 26.7611V28.7485C31.2451 31.5401 29.0465 33.7486 26.2549 33.7486C23.4633 33.7486 21.2524 31.54 21.2524 28.7485V27.5034H22.4976C25.6888 27.5034 28.986 25.8701 29.8169 25.3354ZM23.75 35.809C24.5349 36.0887 25.3758 36.2485 26.2549 36.2485C27.1308 36.2485 27.9682 36.0893 28.75 35.8115V38.5239C28.75 38.5239 28.3682 40.001 26.25 40.001C24.1318 40.001 23.75 38.5239 23.75 38.5239V35.809ZM13.7622 54.9962C10.3213 54.9962 7.50489 57.8028 7.50489 61.2437V62.4984C7.50489 63.0272 7.58977 63.5335 7.71485 64.0244C8.45293 63.8487 9.21833 63.7461 10.0073 63.7461H17.5073C18.2985 63.7461 19.0638 63.8454 19.7974 64.0244C19.922 63.5334 20.0073 63.0273 20.0073 62.4984V61.2437C20.0073 57.8028 17.2031 54.9962 13.7622 54.9962ZM39.9951 54.9962C36.5542 54.9962 33.7402 57.8028 33.7402 61.2437V62.4984C33.7402 63.0283 33.8271 63.535 33.9526 64.0269C34.6884 63.8464 35.4561 63.7461 36.25 63.7461H43.7524C44.5402 63.7461 45.3024 63.8445 46.0327 64.0221C46.157 63.5318 46.2427 63.0265 46.2427 62.4986V61.2438C46.2427 57.8029 43.436 54.9962 39.9951 54.9962ZM66.2476 54.9962C62.8067 54.9962 60 57.8028 60 61.2437V62.4984C60 63.0272 60.0852 63.5335 60.21 64.0244C60.9463 63.8435 61.7151 63.7461 62.5098 63.7461H70.0122C70.7969 63.7461 71.5585 63.8481 72.2925 64.022C72.4171 63.5317 72.5024 63.0265 72.5024 62.4986V61.2437C72.5024 57.8028 69.6884 54.9962 66.2476 54.9962ZM10.0073 66.246C5.87397 66.246 2.50494 69.6128 2.50489 73.7462V76.2485C2.50426 76.4135 2.53629 76.5769 2.59913 76.7295C2.66197 76.882 2.75438 77.0206 2.87103 77.1372C2.98768 77.2539 3.12627 77.3463 3.2788 77.4091C3.43133 77.472 3.5948 77.5041 3.75977 77.5034H23.7549C23.9199 77.5041 24.0833 77.472 24.2359 77.4091C24.3884 77.3463 24.527 77.2539 24.6436 77.1372C24.7603 77.0206 24.8527 76.882 24.9155 76.7295C24.9784 76.5769 25.0104 76.4135 25.0098 76.2485V73.7462C25.0098 69.6128 21.6407 66.246 17.5073 66.246H10.0073ZM36.25 66.246C32.1167 66.246 28.7476 69.6128 28.7476 73.7462V76.2485C28.7469 76.4135 28.779 76.5769 28.8418 76.7295C28.9046 76.882 28.9971 77.0206 29.1137 77.1372C29.2304 77.2539 29.3689 77.3463 29.5215 77.4091C29.674 77.472 29.8375 77.5041 30.0024 77.5034H49.9976C50.1625 77.5041 50.326 77.472 50.4785 77.4091C50.6311 77.3463 50.7697 77.2539 50.8863 77.1372C51.003 77.0206 51.0954 76.882 51.1582 76.7295C51.2211 76.5769 51.2531 76.4135 51.2524 76.2485V73.7462C51.2524 69.6128 47.8858 66.246 43.7524 66.246H36.25ZM62.5098 66.246C58.3764 66.246 55.0073 69.6128 55.0073 73.7462V76.2485C55.0067 76.4135 55.0387 76.5769 55.1016 76.7295C55.1644 76.882 55.2568 77.0206 55.3735 77.1372C55.4901 77.2539 55.6287 77.3463 55.7812 77.4091C55.9338 77.472 56.0972 77.5041 56.2622 77.5034H76.2598C76.5913 77.5021 76.9087 77.3691 77.1422 77.1338C77.3757 76.8984 77.5062 76.58 77.5049 76.2485V73.7462C77.5048 69.6128 74.1456 66.246 70.0122 66.246H62.5098Z" fill="#FDC948"/>
+</svg>
+',
+                ),
+                array(
+                    'title' => '90%',
+                    'txt' => 'Tỉ lệ học sinh tốt nghiệp và đỗ chuyên mỗi năm',
+                    'svg' => '<svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_243_493)">
+<path d="M61.7563 27.448C61.8717 30.5544 66.3296 30.5521 66.4438 27.448V17.7849L61.7563 19.4982V27.448Z" fill="#FDC948"/>
+<path d="M15.0949 13.3564L39.1869 22.1681C39.7068 22.3583 40.2766 22.3583 40.7966 22.1684L64.9048 13.3567C66.9438 12.6137 66.943 9.69653 64.9049 8.95403L40.8048 0.142617C40.2851 -0.0475391 39.7149 -0.0475391 39.1952 0.142617L15.0952 8.95403C13.0565 9.69684 13.0572 12.6139 15.0949 13.3564Z" fill="#FDC948"/>
+<path d="M23.8501 51.1292C23.8501 46.7456 20.2837 43.1792 15.9001 43.1792C5.36773 43.5789 5.3707 58.6811 15.9002 59.0792C20.2837 59.0792 23.8501 55.5128 23.8501 51.1292Z" fill="#FDC948"/>
+<path d="M72.0503 51.1292C72.0503 46.7456 68.4839 43.1792 64.1003 43.1792C53.5679 43.5789 53.5709 58.6811 64.1004 59.0792C68.4839 59.0792 72.0503 55.5128 72.0503 51.1292Z" fill="#FDC948"/>
+<path d="M32.05 51.1292C32.05 55.5128 35.6165 59.0792 40 59.0792C50.5324 58.6795 50.5296 43.5773 39.9999 43.1792C35.6165 43.1792 32.05 46.7456 32.05 51.1292Z" fill="#FDC948"/>
+<path d="M64.1001 59.0793C60.9612 59.0793 58.032 59.9939 55.5645 61.5701C58.6835 65.1751 60.5715 69.8618 60.5715 74.9792V80.0001H77.6563C78.9507 80.0001 80.0001 78.9507 80.0001 77.6564V74.9792C80.0001 66.212 72.8674 59.0793 64.1001 59.0793Z" fill="#FDC948"/>
+<path d="M39.8668 59.0793C31.0349 59.0793 23.8496 66.212 23.8496 74.9793V80.0001H55.8842V74.9792C55.884 66.212 48.6987 59.0793 39.8668 59.0793Z" fill="#FDC948"/>
+<path d="M15.9 59.0793C7.13266 59.0793 0 66.212 0 74.9792V77.6564C0 78.9507 1.04938 80.0001 2.34375 80.0001H19.162V74.9792C19.162 69.8107 21.088 65.0814 24.263 61.4622C21.8319 59.9526 18.9661 59.0793 15.9 59.0793Z" fill="#FDC948"/>
+<path d="M54.8958 30.3891V22.0059L42.4058 26.571C40.8583 27.1405 39.1213 27.1405 37.575 26.5696L25.1042 22.0085V30.3916C25.1042 31.3757 25.7189 32.2549 26.643 32.5927L39.1871 37.1806C39.7067 37.3706 40.2769 37.3706 40.7966 37.1807L53.3566 32.5906C54.2808 32.2527 54.8958 31.3734 54.8958 30.3891Z" fill="#FDC948"/>
+</g>
+<defs>
+<clipPath id="clip0_243_493">
+<rect width="80" height="80" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+',
+                ),
+                array(
+                    'title' => '50+',
+                    'txt' => 'Giảng viên có trình độ và kinh nghiệm tới từ các trường danh tiếng',
+                    'svg' => '<svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M61.5597 59.0348H65.8336V60.3555H61.5597V59.0348ZM61.5597 56.3853H67.7285V57.706H61.5597V56.3853ZM61.5597 53.7439H67.7285V55.0646H61.5597V53.7439ZM58.8855 49.2978V66.5244H70.3699V49.2978H58.8855ZM45.6949 49.2978V66.5244H57.1793V49.2978H45.6949ZM43.0535 46.6975H73.0441V69.1658H61.5597C61.5575 69.5168 61.4166 69.8527 61.1676 70.1002C60.9187 70.3476 60.5819 70.4865 60.2309 70.4866H55.8257C55.4812 70.478 55.1536 70.3351 54.913 70.0883C54.6723 69.8416 54.5377 69.5105 54.5379 69.1658H43.0535V46.6975ZM43.8738 36.8538C47.5441 36.8671 51.1924 37.4199 54.7019 38.4944C59.0672 39.6464 63.1828 41.5929 66.8425 44.2366H40.5926V63.1038H19.5023C20.8965 62.3886 22.0129 61.2296 22.6754 59.8096C23.3378 58.3896 23.5087 56.7895 23.161 55.2617C22.8196 53.9755 22.1165 52.8143 21.1351 51.9157C20.1536 51.0171 18.9351 50.4188 17.6239 50.1918V49.9788C17.645 48.9639 17.8915 47.9664 18.3455 47.0584C18.7995 46.1505 19.4496 45.3548 20.2489 44.729C24.1308 41.9413 28.458 39.8332 33.0458 38.4944C36.5662 37.4687 40.2076 36.917 43.8738 36.8538ZM6.74226 28.8269C6.94899 28.7584 7.17444 28.7749 7.36904 28.8726C7.56365 28.9704 7.71147 29.1414 7.78 29.3481L15.2121 51.7753C15.9058 51.6166 16.6242 51.5982 17.3251 51.7214C18.026 51.8446 18.6952 52.1069 19.2932 52.4927C19.8911 52.8785 20.4059 53.3801 20.807 53.9679C21.2081 54.5557 21.4875 55.2179 21.6288 55.9154C21.77 56.6128 21.7702 57.3315 21.6294 58.0291C21.4886 58.7267 21.2096 59.389 20.8089 59.9771C20.4081 60.5652 19.8937 61.0671 19.296 61.4533C18.6983 61.8395 18.0293 62.1021 17.3285 62.2258C14.2523 62.9889 12.5543 63.4156 11.2582 58.2476C10.1343 53.7193 11.291 52.8253 13.6535 52.161L6.22141 29.8645C6.18748 29.7622 6.17403 29.6542 6.18185 29.5466C6.18966 29.4391 6.21858 29.3342 6.26695 29.2378C6.31532 29.1415 6.3822 29.0556 6.46376 28.9851C6.54532 28.9146 6.63992 28.8608 6.74226 28.8269ZM43.8738 7.32251C46.4697 7.32251 49.0073 8.09228 51.1657 9.53447C53.3241 10.9767 55.0063 13.0265 55.9997 15.4248C56.9931 17.8231 57.253 20.4621 56.7466 23.0081C56.2402 25.5541 54.9901 27.8927 53.1546 29.7283C51.319 31.5639 48.9804 32.8139 46.4344 33.3203C43.8884 33.8267 41.2494 33.5668 38.8511 32.5734C36.4528 31.58 34.403 29.8978 32.9608 27.7394C31.5186 25.581 30.7488 23.0434 30.7488 20.4475C30.7488 16.9665 32.1316 13.6281 34.593 11.1667C37.0544 8.70532 40.3928 7.32251 43.8738 7.32251Z" fill="#FDC948"/>
+</svg>
+',
+                ),
+            );
+            foreach ($data as $k=>$v): ?>
+            <div>
+                <div class="uk-text-center">
+                    <div>
+                        <?= $v['svg'] ?>
+                    </div>
+                    <div class="uk-margin about__thanhtuu__title"><?= $v['title'] ?></div>
+                    <div class="about__thanhtuu__txt"><?= $v['txt'] ?></div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
+<!--/Thành tựu nổi bật và đáng tự hào-->
+
+<!--Những dấu ấn trong hành trình của chúng tôi-->
+<div class="uk-section home__section">
+    <div class="uk-container">
+        <h2 class="uk-h2 uk-text-center about__title">Những dấu ấn trong hành trình của chúng tôi</h2>
+    </div>
+</div>
+<!--/Những dấu ấn trong hành trình của chúng tôi-->
+
+
+<?php require "template-parts/layouts/footer.php"; ?>
--- js/app.js
+++ js/app.js
@@ -2,3 +2,66 @@ console.log(
   "%c" + "W E L C O M E   T O   C V . C A T H T M L C S S . N E T",
   "font-family:Montserrat ; font-size:14px; background: #009999; border-radius: 2px; padding: 6px 12px; margin: 5px 10px 7px 0px; color: #ffffff;"
 );
+
+const x = document.querySelector.bind(document);
+const xx = document.querySelectorAll.bind(document);
+
+const app = {
+    render: function () {
+        const app = x('#app');
+        const header = x('.header');
+        const about__section1 = x('.about__section1');
+
+        const stickyHeader = () => {
+            if (header){
+                UIkit.sticky(header, {
+
+                });
+            }
+        }
+
+        const stickyBreadcrumd = () => {
+            if (about__section1){
+                const h_header = header ? header.offsetHeight + 'px' : 0
+                console.log('height Header', h_header);
+                UIkit.sticky(about__section1, {
+                    'offset': h_header
+                });
+            }
+        }
+
+        stickyHeader()
+        stickyBreadcrumd()
+
+        onresize = (event) => {
+            console.log('onresize')
+            stickyHeader()
+            stickyBreadcrumd()
+        };
+
+        document.onscroll = function () {
+            let scrollTop = window.scrollY || document.documentElement.scrollTop;
+            console.log('scrollTop',scrollTop);
+            stickyHeader()
+            stickyBreadcrumd()
+            if (scrollTop > 0){
+
+            }else {
+
+            }
+        }
+    },
+    start: function () {
+        this.render();
+    }
+}
+
+window.addEventListener("load", ()=>{
+    console.log("page is fully loaded");
+    app.start();
+})
+
+
+$(window).on("load", function (e) {
+
+});
